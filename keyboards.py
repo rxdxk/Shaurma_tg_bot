eb07fe366131e2dc51225f344e44589c1a22a141
Keklovepython <85251714+Keklovepython@users.noreply.github.com>
2023-07-03 20:33:33 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -4,20 +4,6 @@ start_kb = types.InlineKeyboardMarkup()
 butn1 = types.InlineKeyboardButton("Наша адреса",url=r'https://www.google.com/maps/place/%D1%83%D0%BB.+%D0%90%D0%BD%D0%BD%D1%8B+%D0%90%D1%85%D0%BC%D0%B0%D1%82%D0%BE%D0%B2%D0%BE%D0%B9,+1,+%D0%9A%D0%B8%D0%B5%D0%B2,+02000/@50.4128532,30.6407127,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4c5015f137b37:0xaf22765b5226e0a9!8m2!3d50.4128498!4d30.6432876!16s%2Fg%2F11lpc_7s3p?authuser=0&entry=ttu')
 start_kb.add(butn1)
 
-main_menu_buttons = [types.KeyboardButton("Контакти☎️"),
-                     types.KeyboardButton("Меню📋")]
+main_menu_buttons = [types.KeyboardButton("Контакти"),
+                     types.KeyboardButton("Меню")]
 main_menu_kb = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*main_menu_buttons)
-
-
-food_menu_buttons = [types.KeyboardButton("Шаурма🌯"),
-                     types.KeyboardButton("Фалафель🧆"),
-                     types.KeyboardButton("Картопля фрі🍟"),
-                     types.KeyboardButton("Напої🥤"),
-                     types.KeyboardButton("Назад↩️")]
-food_menu_kb = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2).add(*food_menu_buttons)
-
-shaurma_buttons = []
-falalel_butoons = []
-fries_buttons = []
-drinks_buttons=  []
-
