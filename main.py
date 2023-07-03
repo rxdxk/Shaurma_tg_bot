79a501a33fc29621c5f22a0f5e775faa4ee4e1d5
Keklovepython <85251714+Keklovepython@users.noreply.github.com>
2023-07-03 19:34:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import telebot
 from telebot import types
+from keyboards import main_menu_kb,food_menu_kb,start_kb
+
+food_menu = False
 
 API_TOKEN = '6094386761:AAFPrt-ZYA0XDzR_54ry637EcHRcnc0vEuA'
 bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['start'])
 def say_hello(message):
-    bot.send_message(message.chat.id,text='Привіт! Вітаємо вас у боті Shaurma Island\nОберіть один з пунктів нижче:')
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton("but1")
-    kb.add(button1)
-    bot.send_message(message.chat.id,"Shaurma island - very well" ,reply_markup=kb)
     with open('stonik.jpg','rb') as file:
         bot.send_photo(message.chat.id,file)
+    bot.send_message(message.chat.id,"Shaurma island - very well" ,reply_markup=start_kb)
+    kb2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    bot.send_message(message.chat.id,'Привіт! Вітаємо вас у боті Shaurma Island\nОберіть один з пунктів нижче:',reply_markup = main_menu_kb)
+  
+
+
+
+
+@bot.message_handler()
+def kb_answer(message):
+   if message.text == 'Контакти':
+       bot.send_message(message.chat.id,"+123456789")
+    
+   elif message.text == 'Меню':
+       bot.send_message(message.chat.id,"Ось наше меню:",reply_markup=food_menu_kb)
+       global food_menu
+       food_menu = True
+   elif message.text == 'Назад':
+       if food_menu == True:
+           bot.send_message(message.chat.id,"Ви повернулись назад",reply_markup=main_menu_kb)
     
 
-
+    
 
 bot.polling()
+
+
