b58e8543c0ec9cf8e14a0a9a975dc7415540c50f
rxdxk <116183763+rxdxk@users.noreply.github.com>
2023-07-03 20:07:18 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conn = sqlite3.connect('mydatabase.db')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS orders
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  first_name TEXT,
+                  last_name TEXT,
+                  number INTEGER,
+                  address TEXT)''')
+
+
+conn.execute("INSERT INTO orders (first_name, last_name, order_number, address) VALUES (?, ?, ?, ?)",
+             (first_name, last_name, number, address))
+
+conn.commit()
+
+cursor = conn.execute("SELECT * FROM orders")
+for row in cursor:
+    print(row)
+
+conn.close()
