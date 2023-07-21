c2af4e4a7f7a0c865c5392a1e6ef9c31dda82e6f
rxdxk <116183763+rxdxk@users.noreply.github.com>
2023-07-21 14:47:49 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 import sqlite3
 from telebot import TeleBot
+from dotenv import load_dotenv
+load_dotenv()
 
-bot = TeleBot(Your_token) 
+API_TOKEN = os.getenv('API_TOKEN')
+
+bot = TeleBot(API_TOKEN) 
 group_chat_id=your_chat_id
 
 def create_order(user_data, basket_str, user_num, user_adress, sent,):
