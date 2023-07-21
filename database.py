342645ed863e769b43ef48e0a6f718f8480a4913
rxdxk <116183763+rxdxk@users.noreply.github.com>
2023-07-21 12:10:08 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import sqlite3
+ import sqlite3
 
 def create_order(user_data, basket_str, user_num, user_adress):
     conn = sqlite3.connect('zxc.db')
@@ -20,5 +20,24 @@ def create_order(user_data, basket_str, user_num, user_adress):
     for row in rows:
         with open('zxc.txt', w) as file:
             file.write(str(rows))
+    # Создание подключения к базе данных
+    conn = sqlite3.connect('orders.db')
+    cursor = conn.cursor()
+
+    # Выполнение запроса для получения новых элементов из таблицы
+    cursor.execute('SELECT * FROM orders WHERE sent = 0')
+    new_rows = cursor.fetchall()
+
+    # Отправка новых элементов в группу
+    for row in new_rows:
+        order_id, name = row[0], row[1]
+        message = f"Новый заказ: Order ID: {order_id}, Name: {name}"
+        bot.send_message(group_chat_id, message)
+
+        # Помечаем элемент как отправленный, чтобы не отправить его снова в будущем
+        cursor.execute('UPDATE orders SET sent = 1 WHERE order_id = ?', (order_id,))
+        conn.commit()
+
+    # Закрытие курсора и подключения
     cursor.close()
     conn.close()
