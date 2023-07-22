d7c6f0a006fa7f42871f9978b401c62ac6cc4db3
Keklovepython <85251714+Keklovepython@users.noreply.github.com>
2023-07-22 12:43:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
 import os
 from telebot import TeleBot
 #Импорт базы данных
-import db
 #Импорты клавиатур из  keyboaards.py
 from keyboards import * 
 #Загрузка .env
 from dotenv import load_dotenv
 load_dotenv()
-
 API_TOKEN = os.getenv('API_TOKEN') #Токен из .env
 bot = TeleBot(API_TOKEN) 
 
 my_dict = {}
-
 
 
 
